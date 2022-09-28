612d04a5e3f2dbefc6109b827fc496ea1d05a944
Marc Pesquera <109769991+mpesq@users.noreply.github.com>
2022-09-28 23:28:26 +0200

    add exercise

---
+++ _8kyu/CountByX.java
@@ -0,0 +1,31 @@
+package com._8kyu;
+
+import java.util.ArrayList;
+
+public class CountByX {
+    /*
+    Create a function with two arguments that will return an array of the first n multiples of x.
+
+    Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+    Return the results as an array or list ( depending on language ).
+
+    Examples
+    countBy(1,10)  // should return  {1,2,3,4,5,6,7,8,9,10}
+    countBy(2,5)  // should return {2,4,6,8,10}
+    */
+    public static int[] countBy(int x, int n) {
+        int[] result = new int[n];
+        for(int i = 1; i <= n; i++) {
+            result[i-1] = i * x;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] multip = countBy(2,5);
+        for (int i : multip) {
+            System.out.println(i);
+        }
+    }
+}
