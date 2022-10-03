a2bbdd41e4a58f88624bf8384bc5f81679bbdc4e
Marc Pesquera <109769991+mpesq@users.noreply.github.com>
2022-10-03 13:54:48 +0200

    Add files via upload

---
+++ _7kyu/MinMax.java
@@ -0,0 +1,42 @@
+package com._7kyu;
+
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+public class MinMax {
+
+    /*
+    Story
+    Ben has a very simple idea to make some profit: he buys something and sells it again. Of course,
+    this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead,
+    he's going to buy it for the lowest possible price and sell it at the highest.
+
+    Task
+    Write a function that returns both the minimum and maximum number of the given list/array.
+
+    Examples (Input --> Output)
+    [1,2,3,4,5] --> [1,5]
+    [2334454,5] --> [5,2334454]
+    [1]         --> [1,1]
+    Remarks
+    All arrays or lists will always have at least one element, so you don't need to check the length.
+    Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
+     */
+
+    public static int[] minMax(int[] arr) {
+        OptionalInt optionalIntMax = Arrays.stream(arr).max();
+        OptionalInt optionalIntMin = Arrays.stream(arr).min();
+        int max = optionalIntMax.getAsInt();
+        int min = optionalIntMin.getAsInt();
+        int[] result = {min, max};
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1};
+        int[] minMaxCouple = minMax(numbers);
+        for (int i : minMaxCouple) {
+            System.out.println(i);
+        }
+    }
+}
